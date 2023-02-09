2988c5dab22d8127835f3b732dcb4982ddac7000
kiminha983 <kiminha983@gmail.com>
2023-02-09 15:49:36 +0900

    day19 sort01

--- day16.java
+++ day16.java
@@ -2,6 +2,6 @@ public class day16 {
     public static void main(String[] args) {
         Node node = new Node();
         node.data = "피카츄";
-        System.out.println(node.toString());
+        System.out.println(node);
     }
 }
