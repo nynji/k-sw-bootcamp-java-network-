7062c2b4bb35da275dcc64e640fc8c178854b373
HJ <12210544a@inha.edu>
2023-02-13 13:17:18 +0900

    day21 network url

--- Day21.java
+++ Day21.java
@@ -1,18 +1,26 @@
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 public class Day21 {
     public static void main(String[] args) {
         try {
-            InetAddress addr = InetAddress.getByName("www.inha.ac.kr");
-
-            System.out.println(addr);
-            System.out.println(addr.getCanonicalHostName());
-            System.out.println(addr.getHostAddress());
-            System.out.println(addr.getHostName());
-        } catch (UnknownHostException e) {
-            //throw new RuntimeException(e);
-            System.out.println("해당 URL은 존재하지 않습니다.");
+            URL url = new URL("http://www.inha.ac.kr");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e)
+            // Handle exceptions
+        } catch (IOException ex){
+            System.out.println("입출력오류 ");
         }
     }
 }
