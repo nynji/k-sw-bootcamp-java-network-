7747578d44bf6ced657a63adea8ec5d0e488446f
HJ <12210544a@inha.edu>
2023-02-14 11:16:54 +0900

    day22 SimpleEchoServer v0.3

--- SimpleEchoServer.java
+++ SimpleEchoServer.java
@@ -4,11 +4,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class SimpleEchoServer {
     public static void main(String[] args) {
         System.out.println("에코 서버 시작됨");
-        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+        try (ServerSocket serverSocket = new ServerSocket(7000)) {
             System.out.println("클라이언트 접속 대기 중.....");
             Socket clientSocket = serverSocket.accept();  // 접속 대기
             System.out.println("클라이언트 접속됨.");
@@ -18,16 +21,27 @@ public class SimpleEchoServer {
                  PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true)) {
                 //GIS:클라이언트가 서버에게 보냄
                 //true : 송신용 버퍼를 자동으로 비움
-                String line;
-                while ((line = br.readLine()) != null) {
-                    System.out.println("클라이언트로 부터 받은 메세지 : " + line);
-                    pw.println(line);  // 클라이언트로 송신
-                }
+//                String line;
+//                while ((line = br.readLine()) != null) {
+//                    System.out.println("클라이언트로 부터 받은 메세지 : " + line);
+//                    pw.println(line);  // 클라이언트로 송신
+//                }
+                Stream s = Stream.generate(() -> {
+                    try {
+                        return br.readLine();
+                    } catch (IOException ex) {
+                        return null; //예외처리
+                    }
+                });
+                s.peek(text -> { //코드 줄이기 가능
+                    System.out.println("클라이언트로부터 받은 메세지 : " + text);
+                    pw.println(text);
+                }).allMatch(Objects::nonNull);
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             System.out.println("접속 실패!");
         }
     }
